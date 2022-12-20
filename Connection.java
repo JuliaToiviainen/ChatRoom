6c619aee98caa44459b82fd57c489ade58131639
Julia <63372623+JuliaToiviainen@users.noreply.github.com>
2022-12-20 09:45:52 -0700

    Add files via upload

---
+++ Connection.java
@@ -0,0 +1,31 @@
+
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.io.*;
+
+public class Connection implements Runnable
+{
+	private Socket	client;
+	private static Handler handler = new Handler();
+	private Vector<String> messageQueue;
+	private ArrayList<BufferedWriter> socketConnections;
+	
+	public Connection(Socket client, Vector<String> messageQueue, ArrayList<BufferedWriter> socketConnections) {
+		this.client = client;
+		this.messageQueue = messageQueue;
+		this.socketConnections = socketConnections;
+	}
+
+    /**
+     * This method runs in a separate thread.
+     */	
+	public void run() { 
+		try {
+			handler.process(client, messageQueue, socketConnections);
+		}
+		catch (java.io.IOException ioe) {
+			System.err.println(ioe);
+		}
+	}
+}
